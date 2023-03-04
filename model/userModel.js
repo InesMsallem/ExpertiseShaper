e04c700fedb9578dea99a5fa56750f163a9b76f9
ahmed <ahmedbelghith01@gmail.com>
2023-03-04 15:09:53 +0100

    From ExpertiseBack

--- model/userModel.js
+++ model/userModel.js
@@ -1,29 +1,30 @@
+//Expertise Back
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-    name: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    role: {
-      type: String,
-      enum: ["student", "instructor","admin"]
-    },
-    courses: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "course"
-      }
-    ] 
-  });
-  
-  module.exports = mongoose.model("user", UserSchema);
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"]
+  },
+  courses: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "course"
+    }
+  ]
+});
+
+module.exports = mongoose.model("user", UserSchema);
