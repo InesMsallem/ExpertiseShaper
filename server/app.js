d16e4f47858bc5fbdc6422b95cdf526c9724a0e2
ahmed <ahmedbelghith01@gmail.com>
2023-03-06 17:38:07 +0100

    some updates on resetPassword

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const userRouter = require("./route/userRoute");
+const cors = require("cors");
+const session = require("express-session");
+const dotenv = require('dotenv');
+
+// configure dotenv
+dotenv.config();
+
+// configure session middleware
+app.use(
+  session({
+    secret: "abc1234",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+//connecting to db
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://ahmed:admin@nodeapi.i8w0wza.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("CONNECTED TO DB"))
+  .then(() => app.listen(8080))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/user", userRouter);
---
+++ server/route/userRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/userController");
+const { auth } = require("../middleware/auth");
+router.get("/getUser/:id", userController.getById);
+router.delete("/deleteUser/:_id", auth, userController.deleteUser);
+router.get("/getAllUsers", userController.Userslist);
+router.put("/updateUser/:_id", auth, userController.updateUser);
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
+router.get("/ShowSessionCaptcha", userController.showCaptcha);
+router.get("/generateCaptcha", userController.generateCaptcha);
+
+router.post("/forgot-password", userController.forgotPassword);
+router.post("/reset-password/:resetToken", userController.resetPassword);
+
+module.exports = router;
